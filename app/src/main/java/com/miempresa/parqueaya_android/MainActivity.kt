58a54e7d2d8b2e69f14fd63db1e7b9299b99a1f6
Michell Condori <michell.condori@tecsup.edu.pe>
2023-10-24 18:14:34 -0500

    subien

---
+++ app/src/main/java/com/miempresa/parqueaya_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miempresa.parqueaya_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
